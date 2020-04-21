4912ff76d92a21b27ce902c7d865de3466efc418
rtml <karantank97@gmail.com>
2020-04-21 16:15:38 -0400

    Copy over essential files to the docker

---
+++ copy_files.sh
@@ -0,0 +1,5 @@
+sudo docker cp proc_data.sh darknet_thermal_yolo:home/IR_project
+sudo docker cp run_all_iters.sh darknet_thermal_yolo:home/IR_project
+sudo docker cp thermal_test.data darknet_thermal_yolo:home/IR_project
+sudo docker cp update_docker.sh darknet_thermal_yolo:home/IR_project
+sudo docker cp backup/. darknet_thermal_yolo:/home/IR_project/darknet/build/darknet/x64/backup/
